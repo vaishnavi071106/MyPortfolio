0952c01bed14bc1a700b775fd9cbcc8c83b0ce8f
vaishnavi071106 <ms0804@srmist.edu.in>
2025-10-16 10:38:59 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,8 @@ import Skills from "./Skills.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
 import Certificates from "./Certificates.jsx";
+import HackerRank from "./HackerRank.jsx";
+import Curricula from "./Curricula.jsx";
 
 export {
   Navbar,
@@ -16,4 +18,6 @@ export {
   Projects,
   Contact,
   Certificates,
+  HackerRank,
+  Curricula,
 };
